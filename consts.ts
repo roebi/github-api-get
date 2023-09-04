ddd265e2ebf20647553faca6d8e88b521dc3d69a
Robert Halter <roebi@halter.in>
2023-09-04 19:24:03 +0200

    export const GITHUB_API_BASE_URL

--- consts.ts
+++ consts.ts
@@ -1,9 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your project by using the `import` keyword.
 
-const GITHUB_API_BASE_URL = "https://api.github.com/";
-// const GITHUB_LOGIN = "roebi";
+export const GITHUB_API_BASE_URL = "https://api.github.com/";
 
-// https://github.com/roebi/01-02-Webserver-in-Go-on-Podman
-// const GITHUB_HTML_BASE_URL = "https://github.com/";
+
+// example https://github.com/roebi/01-02-Webserver-in-Go-on-Podman
+export const GITHUB_HTML_BASE_URL = "https://github.com/";
+
+// export const GITHUB_LOGIN = "roebi";
 // export const GITHUB_OWNER_HTML_URL = GITHUB_HTML_BASE_URL + GITHUB_LOGIN + "/";
