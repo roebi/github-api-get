83f32051c586375285819fea1456c9b7381bfaf6
Robert Halter <roebi@halter.in>
2023-09-04 18:07:19 +0200

    intermediate commit: add infoLog Parameter

--- githubApi.ts
+++ githubApi.ts
@@ -5,18 +5,34 @@ import { GITHUB_API_BASE_URL } from "./consts";
 
 const isProd = import.meta.env.PROD; // false;
 
-export async function getGithubTopics(loginName: String, projectName: String): Promise<Array<String>> {
-  // console.group("getGithubTopics");
+export async function getGithubTopics(loginName: String, projectName: String, infoLog: boolean): Promise<Array<String>> {
+  if (infoLog) {
+    console.group("getGithubTopics");
+  }
   if (loginName && projectName) {
-    // console.info('githubApi.ts: loginName: ' + loginName);
-    // console.info('githubApi.ts: projectName: ' + projectName);
-
+    if (infoLog) {
+      console.info('githubApi.ts: loginName: ' + loginName);
+      console.info('githubApi.ts: projectName: ' + projectName);
+    }
     async function returnData(isProd: boolean) {
       if (isProd) {
         // get from github project api
         const GITHUB_API_PROJECT_URL = GITHUB_API_BASE_URL + "repos/" + loginName + "/";
 
         const response = await fetch(GITHUB_API_PROJECT_URL + projectName);
+        if (!response.ok) {
+          if (infoLog) {
+            console.info("API rate limit exceeded for nnn.nnn.nnn.nnn. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)")
+            console.info("https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting")
+          }
+          // Github has a Rate Limit
+          // in this case Status is 403 Forbidden
+          // and return this Message Object / Json:
+          // {
+          //   message: "API rate limit exceeded for nnn.nnn.nnn.nnn. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
+          //   documentation_url: 'https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting'
+          // }
+        }
         return await response.json();
       } else {
         // return mock data
@@ -31,11 +47,15 @@ export async function getGithubTopics(loginName: String, projectName: String): P
         });
       }
     }
-    // console.groupEnd();
+    if (infoLog) {
+      console.groupEnd();
+    }
     // https://www.npmjs.com/package/jsonpath MIT License
     return Promise.resolve(jp.query(await returnData(isProd), "$.topics[*]") as Array<String>);
   } else {
-    // console.groupEnd();
+    if (infoLog) {
+      console.groupEnd();
+    }
     return Promise.resolve([] as Array<String>);
   }
 }
