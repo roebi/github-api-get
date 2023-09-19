7feb841d0d151b9d584759df1c228bb395abc121
Robert Halter <roebi@users.noreply.github.com>
2023-09-19 23:41:27 +0000

    test and prettier

--- githubApiGet.js
+++ githubApiGet.js
@@ -16,26 +16,25 @@ import { GITHUB_API_BASE_URL } from "./consts.js";
  * @returns {Promise<String[]>} The github topics of a project
  */
 export async function getGithubTopics(loginName, projectName, infoLog, isProd) {
+  if (infoLog) {
+    console.group("getGithubTopics");
+  }
+  if (loginName && projectName) {
     if (infoLog) {
-        console.group("getGithubTopics");
+      console.info("githubApi.ts: loginName: " + loginName);
+      console.info("githubApi.ts: projectName: " + projectName);
     }
-    if (loginName && projectName) {
-        if (infoLog) {
-            console.info("githubApi.ts: loginName: " + loginName);
-            console.info("githubApi.ts: projectName: " + projectName);
-        }
-        if (infoLog) {
-            console.groupEnd();
-        }
-        // https://www.npmjs.com/package/jsonpath MIT License
-        return Promise.resolve(jp.query(await returnData(loginName, projectName, infoLog, isProd), "$.topics[*]"));
+    if (infoLog) {
+      console.groupEnd();
     }
-    else {
-        if (infoLog) {
-            console.groupEnd();
-        }
-        return Promise.resolve([]);
+    // https://www.npmjs.com/package/jsonpath MIT License
+    return Promise.resolve(jp.query(await returnData(loginName, projectName, infoLog, isProd), "$.topics[*]"));
+  } else {
+    if (infoLog) {
+      console.groupEnd();
     }
+    return Promise.resolve([]);
+  }
 }
 /**
  * Returns the github the project object / json of a project.
@@ -50,35 +49,36 @@ export async function getGithubTopics(loginName, projectName, infoLog, isProd) {
  * @returns {Promise<any>} The github the project object / json of a project
  */
 async function returnData(loginName, projectName, infoLog, isProd) {
-    if (isProd) {
-        // get from github project api
-        const GITHUB_API_PROJECT_URL = GITHUB_API_BASE_URL + "repos/" + loginName + "/";
-        const response = await fetch(GITHUB_API_PROJECT_URL + projectName);
-        if (!response.ok) {
-            if (infoLog) {
-                console.info("API rate limit exceeded for nnn.nnn.nnn.nnn. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)");
-                console.info("https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting");
-            }
-            // Github has a Rate Limit
-            // in this case Status is 403 Forbidden
-            // and return this Message Object / Json:
-            // {
-            //   message: "API rate limit exceeded for nnn.nnn.nnn.nnn. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
-            //   documentation_url: 'https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting'
-            // }
-        }
-        return await response.json();
+  if (isProd) {
+    // get from github project api
+    const GITHUB_API_PROJECT_URL = GITHUB_API_BASE_URL + "repos/" + loginName + "/";
+    const response = await fetch(GITHUB_API_PROJECT_URL + projectName);
+    if (!response.ok) {
+      if (infoLog) {
+        console.info(
+          "API rate limit exceeded for nnn.nnn.nnn.nnn. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
+        );
+        console.info("https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting");
+      }
+      // Github has a Rate Limit
+      // in this case Status is 403 Forbidden
+      // and return this Message Object / Json:
+      // {
+      //   message: "API rate limit exceeded for nnn.nnn.nnn.nnn. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
+      //   documentation_url: 'https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting'
+      // }
     }
-    else {
-        // return mock data
-        // based on
-        // github-api/01-01-vanilla-HTML5-starter-page.json
-        return Promise.resolve({
-            name: "01-01-vanilla-HTML5-starter-page",
-            html_url: "https://github.com/roebi/01-01-vanilla-HTML5-starter-page",
-            description: "vanilla HTML 5 starter page - Have you ever heard of this HTML 5 tags ?",
-            homepage: "https://roebi.github.io/01-01-vanilla-HTML5-starter-page/",
-            topics: [" mockdata!", "html5", "html5-template", "roebi", "starter"],
-        });
-    }
+    return await response.json();
+  } else {
+    // return mock data
+    // based on
+    // github-api/01-01-vanilla-HTML5-starter-page.json
+    return Promise.resolve({
+      name: "01-01-vanilla-HTML5-starter-page",
+      html_url: "https://github.com/roebi/01-01-vanilla-HTML5-starter-page",
+      description: "vanilla HTML 5 starter page - Have you ever heard of this HTML 5 tags ?",
+      homepage: "https://roebi.github.io/01-01-vanilla-HTML5-starter-page/",
+      topics: [" mockdata!", "html5", "html5-template", "roebi", "starter"],
+    });
+  }
 }
