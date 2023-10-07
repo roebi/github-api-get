d6606cd543d7678008ca453495e71520bdb1cdeb
Robert Halter <roebi@halter.in>
2023-10-07 21:46:56 +0200

    impl and tests of new githubApiGetProject() and new githubApiGetLogin()

--- consts.js
+++ consts.js
@@ -1,7 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your project by using the `import` keyword.
+// pathExpression constant for github-api-get-project
+export const GITHUB_PROJECT_TOPICS = "$.topics[*]";
+// pathExpression constant for github-api-get-login
+export const GITHUB_LOGIN_AVATAR_URL = "$.avatar_url";
+// github constants
+// github api base url
 export const GITHUB_API_BASE_URL = "https://api.github.com/";
+// github HTML base url
 // example https://github.com/roebi/01-02-Webserver-in-Go-on-Podman
 export const GITHUB_HTML_BASE_URL = "https://github.com/";
+// examples
 // export const GITHUB_LOGIN = "roebi";
 // export const GITHUB_OWNER_HTML_URL = GITHUB_HTML_BASE_URL + GITHUB_LOGIN + "/";
--- test.ts
+++ test.ts
@@ -1,5 +1,8 @@
 import test from "ava";
-import { getGithubTopics } from "./githubApiGet.js";
+import { githubApiGetProject, githubApiGetLogin, getGithubTopics } from "./githubApiGet.js";
+import { GITHUB_PROJECT_TOPICS, GITHUB_LOGIN_AVATAR_URL } from "./consts.js";
+
+// dummy tests
 
 test("foo", (t) => {
   t.pass();
@@ -10,14 +13,76 @@ test("bar", async (t) => {
   t.is(await bar, "bar");
 });
 
+// tests for new githubApiGetProject
+
 test("mockdataTopicsWithPathExpression", async (t) => {
   const loginName = "roebi";
   const projectName = "01-01-vanilla-HTML5-starter-page";
   const infoLog = true;
   const isProd = false;
-  const githubTopics = getGithubTopics(loginName, projectName, infoLog, isProd) as Promise<string[]>;
+  const githubTopics = githubApiGetProject(loginName, projectName, GITHUB_PROJECT_TOPICS, infoLog, isProd) as Promise<
+    string[]
+  >;
   t.deepEqual(await githubTopics, [" mockdata!", "html5", "html5-template", "roebi", "starter"]);
 });
+
+test("mockdataProjectInfoEmpty", async (t) => {
+  const loginName = "roebi";
+  const projectName = "01-01-vanilla-HTML5-starter-page";
+  const infoLog = true;
+  const isProd = false;
+  const githubProjectInfoEmtpy = githubApiGetProject("", "", "", infoLog, isProd) as Promise<unknown>;
+  t.deepEqual(await githubProjectInfoEmtpy, {});
+});
+
+test("realdataTopicsWithPathExpression", async (t) => {
+  const loginName = "roebi";
+  const projectName = "01-01-vanilla-HTML5-starter-page";
+  const infoLog = true;
+  const isProd = true;
+  const githubTopics = githubApiGetProject(loginName, projectName, GITHUB_PROJECT_TOPICS, infoLog, isProd) as Promise<
+    string[]
+  >;
+  t.deepEqual(await githubTopics, ["css3", "html5", "html5-template", "roebi", "starter"]);
+});
+
+// tests for new githubApiGetLogin
+
+test("mockdataLoginInfoWithPathExpression", async (t) => {
+  const loginName = "roebi";
+  const infoLog = true;
+  const isProd = false;
+  const githubLoginInfoWithPathExpression = githubApiGetLogin(
+    loginName,
+    GITHUB_LOGIN_AVATAR_URL,
+    infoLog,
+    isProd,
+  ) as Promise<unknown>;
+  t.deepEqual(await githubLoginInfoWithPathExpression, ["https://avatars.githubusercontent.com/u/3611826?v=4"]);
+});
+
+test("mockdataLoginInfoEmpty", async (t) => {
+  const loginName = "";
+  const infoLog = true;
+  const isProd = false;
+  const githubLoginInfoEmtpy = githubApiGetLogin(loginName, "", infoLog, isProd) as Promise<unknown>;
+  t.deepEqual(await githubLoginInfoEmtpy, {});
+});
+
+test("realdataLoginInfoWithPathExpression", async (t) => {
+  const loginName = "roebi";
+  const infoLog = true;
+  const isProd = true;
+  const githubLoginInfoWithPathExpression = githubApiGetLogin(
+    loginName,
+    GITHUB_LOGIN_AVATAR_URL,
+    infoLog,
+    isProd,
+  ) as Promise<unknown>;
+  t.deepEqual(await githubLoginInfoWithPathExpression, ["https://avatars.githubusercontent.com/u/3611826?v=4"]);
+});
+
+// tests for deprecated getGithubTopics
 
 test("mockdataTopics", async (t) => {
   const loginName = "roebi";
